058af5116a283a525a0c2c5e41e0f17ce3b95daf
Arnold Cabrera <Arnold.Cabrera@Norimaconsulting.com>
2020-03-19 08:55:33 +0800

    Update RankedGradeBook.cs
    Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,18 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (averageGrade >= 90)
+                return 'A';
+            else if (averageGrade >= 80)
+                return 'B';
+            else if (averageGrade >= 70)
+                return 'C';
+            else if (averageGrade >= 60)
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
