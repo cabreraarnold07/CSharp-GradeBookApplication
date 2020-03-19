d5e369e3dc1c7c88b842871a3dfe57942d8f7d39
Arnold Cabrera <Arnold.Cabrera@Norimaconsulting.com>
2020-03-19 09:01:33 +0800

    Update RankedGradeBook.cs
    Create GetLetterGrade Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,15 +12,10 @@ namespace GradeBook.GradeBooks
         }
         public override char GetLetterGrade(double averageGrade)
         {
-            if (averageGrade >= 90)
-                return 'A';
-            else if (averageGrade >= 80)
-                return 'B';
-            else if (averageGrade >= 70)
-                return 'C';
-            else if (averageGrade >= 60)
-                return 'D';
-            else
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
                 return 'F';
         }
     }
